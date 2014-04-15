8344f152ee67f1e359b56bc816c33bb7ca122ac0
olyainej <ineevao@gmail.com>
2014-04-15 19:42:44 +0400

    done

--- Homyaki/GAGameController.m
+++ Homyaki/GAGameController.m
@@ -14,9 +14,13 @@
     NSMutableArray* holeButtonsArray;
     NSTimer* gameTimer;
     int timerCallCount;
+    bool isHamsterHere;
+   
+    
 }
 @property (nonatomic, assign) int livesCount;
 @property (nonatomic, weak) IBOutlet UIView* boardView;
+@property (weak, nonatomic) IBOutlet UILabel *lives;
 @end
 
 @implementation GAGameController
@@ -28,6 +32,7 @@
     if (self) {
         _holesCount = holesCount;
     }
+
     return self;
 }
 
@@ -61,8 +66,17 @@
 - (void)updateHolesPosition
 {
     float rowHeight = 60;
-    float colWidth = 55;
-    //задайте правильные позиции кнопок чтобы не вылезали за границы boardView
+    float colWidth = 52;
+    float t = boardView.frame.size.width;
+    int a = t/colWidth;
+    
+    
+    for (int i = 0; i<_holesCount; i++){
+        CGRect frame = CGRectMake((i % a) * colWidth, (i / a) * rowHeight, colWidth, rowHeight);
+        [holeButtonsArray[i] setFrame:frame];
+        [boardView addSubview: holeButtonsArray[i]];
+    }
+    
 }
 
 - (void)setLivesCount:(int)livesCount
@@ -72,7 +86,12 @@
 
 - (void)cleanUp
 {
-    //удаляем все кнопки с доски
+    if (holeButtonsArray){
+    for (int i = 0; i<holeButtonsArray.count; i++){
+        [holeButtonsArray[i] removeFromSuperview];
+    }
+       [holeButtonsArray removeAllObjects ];
+    }
 }
 
 #pragma mark - Game logic
@@ -87,42 +106,73 @@
     timerCallCount = 10;
     holeButtonsArray = [NSMutableArray new];
     CGSize buttonSize = [UIImage imageNamed:@"hamster"].size;
+ 
+    NSString *livesCounter = [NSString stringWithFormat:@"Жизней: %d", _livesCount];
+    self.lives.text = livesCounter;
     for(int i = 0; i < _holesCount; i++){
-        //создаем кнопки
+        UIButton *hole = [[UIButton alloc]init];
+        [hole setImage:[UIImage imageNamed:@"hole.png" ] forState:UIControlStateNormal];
+        holeButtonsArray[i] = hole;
     }
     [self updateHolesPosition];
     
-    gameTimer = [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(gameTimerCall) userInfo:nil repeats:YES];
+    gameTimer = [NSTimer scheduledTimerWithTimeInterval:1.5 target:self selector:@selector(gameTimerCall) userInfo:nil repeats:YES];
 }
 
 - (void)gameTimerCall
 {
-    if(timerCallCount <= 0)
-        [self gameOver:YES];
     timerCallCount--;
+    if (isHamsterHere == YES){
+        [self minusLive];
+    }
     
-    //.........
+    if(self.livesCount == 0)
+        [self gameOver:NO];
     
-    [self randomHamsterArrise];
+    else if(timerCallCount <= 0){
+        [self gameOver:YES];
+    }
+    
+    
+    else [self randomHamsterArrise];
 }
 
 - (BOOL)minusLive
 {
     self.livesCount--;
+    
+    self.lives.text = [@"Жизней: " stringByAppendingString:[NSString stringWithFormat:@"%d", _livesCount]];
     return self.livesCount > 0;
 }
 
 - (void)randomHamsterArrise
 {
+    
     int randomNum = arc4random() % holeButtonsArray.count;
     for(int i = 0; i < holeButtonsArray.count; i++){
-        //...........
+        UIButton *hamster = holeButtonsArray[randomNum];
+        [hamster setImage:[UIImage imageNamed:@"hamster.png"] forState:UIControlStateNormal];
+        isHamsterHere = YES;
+        [holeButtonsArray[randomNum] addTarget:self action:@selector(hamsterPressed:)forControlEvents:UIControlEventTouchUpInside];
     }
 }
 
 - (void)hamsterPressed:(UIButton*)holeButt
 {
-    //...........
+   
+    
+    UIImageView *holeView = [[UIImageView alloc]initWithImage:[UIImage imageNamed: @"hamster.png"]];
+    for (int i = 0; i<holeButtonsArray.count; i++){
+   
+        UIButton *disapp = holeButtonsArray[i];
+        if (CGRectEqualToRect(disapp.frame, holeButt.frame)){
+            [holeButtonsArray[i] setImage:[UIImage imageNamed:@"hole.png"] forState:UIControlStateNormal];
+            isHamsterHere = FALSE;
+          
+        }
+    }
+   
+    
 }
 
 - (void)gameOver:(BOOL)isWin
@@ -137,7 +187,9 @@
                                                        delegate:nil
                                               cancelButtonTitle:okTitle
                                               otherButtonTitles:nil];
+    
     [alertView show];
+    [self cleanUp];
 }
 
 - (IBAction)backPressed:(id)sender
